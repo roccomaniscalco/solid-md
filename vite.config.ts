d007d975abdf6c7985e9be15ab30702ed787dded
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-03-02 22:03:11 -0500

    pull alias from tsconfig in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite"
 import solid from "vite-plugin-solid"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [solid()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  plugins: [solid(), tsconfigPaths()],
 })
