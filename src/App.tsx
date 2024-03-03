405a853d68010055b2534eb315b943df4401dd81
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-03-02 19:50:12 -0500

    remove resize handle

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           </TextField.Label>
           <TextField.TextArea
             autoResize
-            class="w-full rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white outline-none ring-offset-2 ring-offset-gray-950 focus:ring-2 focus:ring-teal-400"
+            class="w-full resize-none rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white outline-none ring-offset-2 ring-offset-gray-950 focus:ring-2 focus:ring-teal-400"
           />
           <TextField.Description class="text-sm text-gray-400">
             Everyone can view and edit the content of this text area.
