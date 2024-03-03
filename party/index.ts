ee9f553061553d3d00b1a4454e3221bcf767e924
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-03-03 00:53:47 -0500

    implement naive message persistence

--- party/index.ts
+++ party/index.ts
@@ -1,7 +1,11 @@
 import type * as Party from "partykit/server"
 
 export default class Server implements Party.Server {
-  constructor(readonly room: Party.Room) {}
+  messages: string[]
+
+  constructor(readonly room: Party.Room) {
+    this.messages = []
+  }
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
     // A websocket just connected!
@@ -12,13 +16,17 @@ export default class Server implements Party.Server {
   url: ${new URL(ctx.request.url).pathname}`,
     )
 
-    // let's send a message to the connection
-    conn.send("hello from server")
+    // let's send messages to the connection
+    for (const message of this.messages) {
+      conn.send(message)
+    }
   }
 
   onMessage(message: string, sender: Party.Connection) {
     // let's log the message
     console.log(`connection ${sender.id} sent message: ${message}`)
+    // push it to the messages array
+    this.messages.push(`${sender.id}: ${message}`)
     // as well as broadcast it to all the other connections in the room...
     this.room.broadcast(
       `${sender.id}: ${message}`,
