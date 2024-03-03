0ed168cf5ca2674fa30cb4d6498c076a52e2ec79
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-03-03 04:44:29 -0500

    simplify and optimize
    - remove `draft` signal
    - remove smooth scroll
    - scroll whenever `texts` changes (not just when 1 is sent)

--- src/room.tsx
+++ src/room.tsx
@@ -2,28 +2,23 @@ import { TextMessage, messageSchema } from "@/party/schema"
 import { TextField } from "@kobalte/core"
 import { useParams } from "@solidjs/router"
 import PartySocket from "partysocket"
-import { For, createEffect, createSignal } from "solid-js"
+import { For, createEffect, createSignal, on } from "solid-js"
 
 export default function Room() {
   const { roomId } = useParams()
   const { texts, sendText } = createChatRoom(roomId)
-  const [draft, setDraft] = createSignal("")
-  let chatContainer: HTMLDivElement | undefined
+  let chatRef: HTMLDivElement | undefined
 
-  function submitDraft() {
-    if (!draft()) return
-    sendText(draft())
-    setDraft("")
-    chatContainer?.scrollTo({
-      behavior: "smooth",
-      top: chatContainer.scrollHeight,
-    })
-  }
+  createEffect(
+    on([texts], () => {
+      chatRef?.scrollTo({ top: chatRef?.scrollHeight })
+    }),
+  )
 
   return (
     <>
       <div class="mx-auto flex h-screen max-w-2xl flex-col gap-6 p-10">
-        <div class="flex-1 overflow-auto" ref={chatContainer}>
+        <div class="flex-1 overflow-auto" ref={chatRef}>
           <For each={texts()} fallback={<div>No messages</div>}>
             {(text) => (
               <div>
@@ -36,14 +31,12 @@ export default function Room() {
         <TextField.Root class="flex flex-col gap-2">
           <TextField.TextArea
             class="w-full resize-none rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white outline-none ring-offset-2 ring-offset-gray-950 placeholder:text-gray-500 focus:ring-2 focus:ring-teal-400"
-            onInput={(e) => setDraft(e.target.value)}
-            value={draft()}
             placeholder="Type a message..."
             autoResize
             onKeyDown={(e) => {
               if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault()
-                submitDraft()
+                sendText(e.currentTarget.value)
+                e.currentTarget.value = ""
               }
             }}
           />
