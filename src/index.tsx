0e144940b25e25d498654f367498431268424f97
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-03-05 02:09:52 -0500

    install the devtools (works about 60% of the time)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import { render } from "solid-js/web"
 import App from "@/app"
 import "@/index.css"
+import "solid-devtools"
 
 const root = document.getElementById("root")
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite"
 import solid from "vite-plugin-solid"
+import devtools from "solid-devtools/vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [solid(), tsconfigPaths()],
+  plugins: [solid(), devtools(), tsconfigPaths()],
 })
